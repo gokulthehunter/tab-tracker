bdd2fa211f94bc9eedd6037cc6c56e6e86107fec
gokul <gokul@22feettribalww.com>
2018-05-28 14:36:46 +0530

    from-office

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,20 @@
-const express = require('express') 
-const bodyParser = require('body-parser') 
-const cors = require('cors') 
-const morgan = require('morgan') 
-const {sequelize} = require('./models')
-const config = require('./config/config')
+console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require("morgan")
 
 const app = express()
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-require('./routes')(app)
-
-sequelize.sync()
-    .then(() =>{
-        app.listen(config.port)
-        console.log(`server started on port ${config.port}`)
-        // app.listen(process.env.PORT || 8081)
+app.get('/status', (req, res) => {
+    res.send({
+        message:'hello World'
     })
+})
 
+app.listen(process.env.PORT || 8081)
 
